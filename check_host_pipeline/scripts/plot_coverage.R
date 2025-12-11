1e684bd635e930c584e20e0165cb3a2ad36ef21c
Jodie <jomojaco@ucsc.edu>
2025-12-10 16:11:57 -0800

    change config and plot script to use specific colors

--- check_host_pipeline/scripts/plot_coverage.R
+++ check_host_pipeline/scripts/plot_coverage.R
@@ -6,25 +6,32 @@ library(dplyr)
 # Read data
 data <- read.table(snakemake@input[[1]], header=TRUE, sep="\t")
 
+# Get colors from config
+genome_colors <- unlist(snakemake@config$genome_colors)
+
 # Create bar plot
-p1 <- ggplot(data, aes(x=reorder(organism, -percentage), y=percentage)) +
-  geom_bar(stat="identity", fill="steelblue") +
-  geom_text(aes(label=sprintf("%.1f%%", percentage)), 
+p1 <- ggplot(data, aes(x=reorder(organism, -percentage), y=percentage, fill=organism)) +
+  geom_bar(stat="identity") +
+  scale_fill_manual(values=genome_colors) +
+  geom_text(aes(label=sprintf("%.1f%%", percentage)),
             vjust=-0.5, size=3.5) +
   theme_bw() +
   labs(title="Read Distribution Across References",
        x="Organism", y="Percentage of Mapped Reads") +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
+  theme(axis.text.x=element_text(angle=45, hjust=1),
+        legend.position="none")
 
 # Coverage plot
-p2 <- ggplot(data, aes(x=reorder(organism, -coverage), y=coverage)) +
-  geom_bar(stat="identity", fill="darkgreen") +
-  geom_text(aes(label=sprintf("%.1fx", coverage)), 
+p2 <- ggplot(data, aes(x=reorder(organism, -coverage), y=coverage, fill=organism)) +
+  geom_bar(stat="identity") +
+  scale_fill_manual(values=genome_colors) +
+  geom_text(aes(label=sprintf("%.1fx", coverage)),
             vjust=-0.5, size=3.5) +
   theme_bw() +
   labs(title="Estimated Coverage",
        x="Organism", y="Coverage (x)") +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) +
+  theme(axis.text.x=element_text(angle=45, hjust=1),
+        legend.position="none") +
   scale_y_log10()
 
 # Combine plots
